37b3eb39b461067a31f7599fa7cf3c1e801739d5
Sangoule <sangoule.ndao@uadb.edu.sn>
2022-04-09 03:06:23 +0200

    Tontine004

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,10 +6,27 @@ class Home extends BaseController
 {
     public function index()
     {
-        return view('welcome_message');
+        echo view('layout/entete.php');
+        echo view('welcome_message');
+        echo view('layout/pied.php');
+
     }
     public function contact()
     {
-        return view('contact');
+        echo view('layout/entete.php');
+        echo view('contact');
+        echo view('layout/pied.php'); 
+    }
+    public function connexion()
+    {
+        echo view('layout/entete.php');
+        echo view('utilisateur/connexion');
+        echo view('layout/pied.php');  
+    }
+    public function presentation()
+    {
+        echo view('layout/entete.php');
+        echo view('utilisateur/presentation');
+        echo view('layout/pied.php');  
     }
 }
---
+++ app/Views/layout/pied.php
@@ -0,0 +1,11 @@
+</main>
+  <footer class="pt-5 my-5 text-muted border-top">
+    Created by Fallou toure en &middot; &copy; 2022
+  </footer>
+</div>
+
+	<?= script_tag("js/bootstrap.bundle.min.js") ?>
+
+      
+  </body>
+</html>
