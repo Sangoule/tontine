ddc9af84ed11cf5b1b9517111848207dc1e72dc7
Fallou TOURE <fallou.toure@uadb.edu.sn>
2022-04-09 23:44:51 +0200

    your commit message6

--- app/Views/layout/entete.php
+++ app/Views/layout/entete.php
@@ -21,6 +21,32 @@
 
 
     <style>
+      .form-signin {
+          width: 100%;
+          max-width: 330px;
+          padding: 15px;
+          margin: auto;
+        }
+
+        .form-signin .checkbox {
+          font-weight: 400;
+        }
+
+        .form-signin .form-floating:focus-within {
+          z-index: 2;
+        }
+
+        .form-signin input[type="email"] {
+          margin-bottom: -1px;
+          border-bottom-right-radius: 0;
+          border-bottom-left-radius: 0;
+        }
+
+        .form-signin input[type="password"] {
+          margin-bottom: 10px;
+          border-top-left-radius: 0;
+          border-top-right-radius: 0;
+        }
       .bd-placeholder-img {
         font-size: 1.125rem;
         text-anchor: middle;
@@ -54,6 +80,6 @@
         <li class="nav-item"><a href="<?= base_url() ?>/utilisateur/index" class="nav-link">connexion </a></li>
         <li class="nav-item"><a href="<?= base_url() ?>/utilisateur/presentation" class="nav-link">About</a></li>
       </ul>
-    </header>
+</header>
 
-  <main>
+  <main class="<?= $menuActif=="connexion"?"form-signin text-center":"" ?>">
